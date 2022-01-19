dcc35f08636ce7e785edacd36c2e1432ceeabc3e
Roy <40725093+roy1337@users.noreply.github.com>
2022-01-19 10:55:30 +0200

    Update CustomQRCode.py

--- CustomQRCode.py
+++ CustomQRCode.py
@@ -1,6 +1,6 @@
-## With qrcode module we can make a custom QR code for a website/text easily
-## Make sure to pip install qrcode
-## https://pypi.org/project/qrcode/
+# With qrcode module we can make a custom QR code for a website/text easily
+# Make sure to pip install qrcode
+# https://pypi.org/project/qrcode/
 
 import qrcode
 codeimg = qrcode.make("http://www.google.com")
