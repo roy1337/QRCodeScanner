984c5afb67d6bc3ed2bb9a11c419b9bac44ea925
Roy <40725093+roy1337@users.noreply.github.com>
2022-01-19 10:48:14 +0200

    Create 3linesproject.py

---
+++ 3linesproject.py
@@ -0,0 +1,7 @@
+## With qrcode module we can make a custom QR code for a website/text easily
+## Make sure to pip install qrcode
+## https://pypi.org/project/qrcode/
+
+import qrcode
+codeimg = qrcode.make("http://www.google.com")
+codeimg.save("QRimg.jpeg")
